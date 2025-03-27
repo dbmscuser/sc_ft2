c564c908de38d97bf78a4c23b23a9793e574f264
dbmscuser <dbmscuser>
2025-03-27 18:27:19 +0000

    Added a column; TaskID: PCK-8

--- dbo/Tables/Creation-Scripts/tblName.sql
+++ dbo/Tables/Creation-Scripts/tblName.sql
@@ -1,6 +1,7 @@
 CREATE TABLE [dbo].[tblName] (
-    [Id]       INT           IDENTITY (1, 1) NOT NULL,
-    [Name1]    VARCHAR (MAX) NULL,
-    [Name2]    VARCHAR (50)  NULL,
-    [RobIsOld] BIGINT        NULL
+    [Id]           INT           IDENTITY (1, 1) NOT NULL,
+    [Name1]        VARCHAR (MAX) NULL,
+    [Name2]        VARCHAR (50)  NULL,
+    [RobIsOld]     BIGINT        NULL,
+    [RobIsVeryOld] BIGINT        NULL
 );
